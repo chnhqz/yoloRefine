448d25a981c09cb3f386d7267bb99c7430be73e9
chnhqz <chnhqz55@gmail.com>
2023-11-20 18:51:36 +0800

    添加样例代码

---
+++ code/手写数字识别/code/model.py
@@ -0,0 +1,30 @@
+import torch
+
+class Net(torch.nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.model = torch.nn.Sequential(
+            # The size of the picture is 28x28
+            torch.nn.Conv2d(in_channels=1, out_channels=16, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # The size of the picture is 14x14
+            torch.nn.Conv2d(in_channels=16, out_channels=32, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+            torch.nn.MaxPool2d(kernel_size=2, stride=2),
+
+            # The size of the picture is 7x7
+            torch.nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, stride=1, padding=1),
+            torch.nn.ReLU(),
+
+            torch.nn.Flatten(),
+            torch.nn.Linear(in_features=7 * 7 * 64, out_features=128),
+            torch.nn.ReLU(),
+            torch.nn.Linear(in_features=128, out_features=10),
+            torch.nn.Softmax(dim=1)
+        )
+
+    def forward(self, input):
+        output = self.model(input)
+        return output
